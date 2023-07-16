1d11c81604310b0a05626dcfff823026f053b11b
Soumen Ghorai <soumenghorai2802@gmail.com>
2023-07-17 02:25:24 +0530

    added mongo db connection

---
+++ configs/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    DB_NAME: "movieBooking_db",
+    DB_URL: "mongodb://localhost/movieBooking_db"
+}
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require ("express");
 const bodyParser = require ("body-parser");
 const serverConfig = require('./configs/server.config');
+const dbConfig = require('./configs/db.config')
+const mongoose = require('mongoose');
 
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect(dbConfig.DB_URL)
+const db = mongoose.connection
+db.on("error", () => {
+    console.log("error while connecting to DB");
+})
+db.once("open", () => {
+    console.log("Connected to mongo DB");
+})
+    
 
 require('./routes/movie.route')(app);
 
